9dde0e9e8fc35da210ac2e7ad8bbfedbfc0b13a5
exoarn <nickpe@gmail.com>
2015-03-24 21:46:16 +0100

    Change Quad name to match class names in pieces.lua
    This way they are accessible using Chessimg[<color>][<piece name>]

--- chessimg.lua
+++ chessimg.lua
@@ -4,18 +4,18 @@ ChessImg.white = {}
 ChessImg.black = {}
 ChessImg.img = love.graphics.newImage("Images/Pieces_tiled.png")
 
-ChessImg.white.rook   = love.graphics.newQuad(0   ,11 , 64, 64, img:getDimensions())
-ChessImg.white.bishop = love.graphics.newQuad(59  ,11 , 64, 64, img:getDimensions())
-ChessImg.white.queen  = love.graphics.newQuad(119 ,11 , 64, 64, img:getDimensions())
-ChessImg.white.king   = love.graphics.newQuad(180 ,11 , 64, 64, img:getDimensions())
-ChessImg.white.knight = love.graphics.newQuad(241 ,11 , 64, 64, img:getDimensions())
-ChessImg.white.pawn   = love.graphics.newQuad(300 ,11 , 64, 64, img:getDimensions())
+ChessImg.white.Rook   = love.graphics.newQuad(0   ,11 , 64, 64, ChessImg.img:getDimensions())
+ChessImg.white.Bishop = love.graphics.newQuad(59  ,11 , 64, 64, ChessImg.img:getDimensions())
+ChessImg.white.Queen  = love.graphics.newQuad(119 ,11 , 64, 64, ChessImg.img:getDimensions())
+ChessImg.white.King   = love.graphics.newQuad(180 ,11 , 64, 64, ChessImg.img:getDimensions())
+ChessImg.white.Knight = love.graphics.newQuad(241 ,11 , 64, 64, ChessImg.img:getDimensions())
+ChessImg.white.Pawn   = love.graphics.newQuad(300 ,11 , 64, 64, ChessImg.img:getDimensions())
 
-ChessImg.black.rook   = love.graphics.newQuad(0   ,85 , 64, 64, img:getDimensions())
-ChessImg.black.bishop = love.graphics.newQuad(59  ,85 , 64, 64, img:getDimensions())
-ChessImg.black.queen  = love.graphics.newQuad(119 ,85 , 64, 64, img:getDimensions())
-ChessImg.black.king   = love.graphics.newQuad(180 ,85 , 64, 64, img:getDimensions())
-ChessImg.black.knight = love.graphics.newQuad(241 ,85 , 64, 64, img:getDimensions())
-ChessImg.black.pawn   = love.graphics.newQuad(300 ,85 , 64, 64, img:getDimensions())
+ChessImg.black.Rook   = love.graphics.newQuad(0   ,85 , 64, 64, ChessImg.img:getDimensions())
+ChessImg.black.Bishop = love.graphics.newQuad(59  ,85 , 64, 64, ChessImg.img:getDimensions())
+ChessImg.black.Queen  = love.graphics.newQuad(119 ,85 , 64, 64, ChessImg.img:getDimensions())
+ChessImg.black.King   = love.graphics.newQuad(180 ,85 , 64, 64, ChessImg.img:getDimensions())
+ChessImg.black.Knight = love.graphics.newQuad(241 ,85 , 64, 64, ChessImg.img:getDimensions())
+ChessImg.black.Pawn   = love.graphics.newQuad(300 ,85 , 64, 64, ChessImg.img:getDimensions())
 
 return ChessImg
