8bf8b3ee6988ee0d293a3f4bd89023ccab25e99f
Nick Peters <nickpe@gmail.com>
2015-03-26 20:14:35 +0100

    Remove trailing whitespaces

--- board.lua
+++ board.lua
@@ -18,7 +18,7 @@ function Board.new()
 			self[i][j] = tile
 		end
 	end
-	
+
 	function self.fill()
 		order_of_pieces = { "Rook", "Knight", "Bishop", "Queen",
 							"King", "Bishop", "Knight", "Rook"}
@@ -64,9 +64,9 @@ function Board.new()
 			for j = 1, #self[i] do
 				if self[i][j].occupant then
 					local piece = {}
-					piece.identity, piece.color = 
+					piece.identity, piece.color =
 								self[i][j].occupant.identify()
-					piece.quad = ChessImg[piece.color][piece.identity] 
+					piece.quad = ChessImg[piece.color][piece.identity]
 					love.graphics.draw(ChessImg.img, piece.quad,
 									((i-1)*square_size + x),
 									((j-1)*square_size + y))
@@ -97,21 +97,21 @@ function Board.new()
 	function self.draw_tiles(start_x, start_y)
 		local dark_rgb  = {0, 100, 100} --rgb value
 		local light_rgb = {0, 200, 200}
-		
+
 		local x = start_x or 0
 		local y = start_y or 0
 
-		
+
 		for i = 1, #self do
 			for j = 1, #self[i] do
-				
+
 				if self[i][j].color == "dark" then
 					love.graphics.setColor(unpack(dark_rgb))
 				else
 					love.graphics.setColor(unpack(light_rgb))
 				end
 
-				love.graphics.rectangle('fill', x, y, 
+				love.graphics.rectangle('fill', x, y,
 								square_size, square_size);
 				x = x + square_size
 			end
@@ -121,10 +121,10 @@ function Board.new()
 
 		end
 	end
-	
-	
-	
-	
+
+
+
+
 	self.fill()
 
 	return self
