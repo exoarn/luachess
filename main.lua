efde1cd028c73e5bf47d82ac6b63eefcaabf57c9
exoarn <nickpe@gmail.com>
2015-03-28 14:02:25 +0100

    Add mouseclick testing function
    licking the left mouse button while on a tile with a piece
    now highlights or unhighlights that tile.

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 Pieces = require("pieces")
 Board_module = require("board")
 
+selected piece = false
+
 function love.load()
     Board = Board_module.new()
 
@@ -12,6 +14,27 @@ end
 
 function love.update(dt)
 end
+
+function love.mousepressed(x, y, button)
+	if button == 'l' then
+		local x_offset, y_offset = Board.get_board_offset()
+		local tilesize = Board.get_tilesize()
+		
+		local i = math.ceil((x - x_offset) / tilesize)
+		local j = math.ceil((y - y_offset) / tilesize)
+
+		print("Mousepress,\tx: " .. tostring(x) .. ", y: " .. tostring(y) .. "\n\t\ti: " .. tostring(i) .. ", j:" .. tostring(j))
+		
+		print("occupant: ",Board[i][j].occupant) 
+		
+		if (i <= 8 and j <= 8) and Board[i][j].occupant then
+			print("Highlight: ", Board[i][j].highlight )
+			Board[i][j].highlight = not Board[i][j].highlight  
+		end
+
+	end
+end
+
 
 function love.draw()
     Board.draw()
